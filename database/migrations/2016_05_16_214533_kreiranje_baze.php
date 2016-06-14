2bc27f008bc9aaf56eee5c4c4ab049ecd1d2a620
Saša Jović <jovicsasajovic@gmail.com>
2016-06-14 20:25:57 +0200

    0.0.2
    Generisam kod za kreiranje sledećih tabela:
    templejt (id, naziv),
    vrsta_objekta (id, naziv),
    grad (id, naziv),
    objekat (id, korisnik_id, templejt_id, grad_id, vrsta_objekta_id, naziv,
    slug, opis, adresa, galerija, telefon, email, x, y, z),
    vrsta_smestaja (id, naziv),
    vrsta_kapaciteta (id, naziv),
    dodaci (id, naziv, tag),
    smestaj (id, objekat_id, vrsta_smestaja_id, vrsta_kapaciteta_id,
    dodaci),
    rezervacija (id, korisnik_id, smestaj_id, datum_prijave, datum_odjave)

--- database/migrations/2016_05_16_214533_kreiranje_baze.php
+++ database/migrations/2016_05_16_214533_kreiranje_baze.php
@@ -30,11 +30,105 @@ class KreiranjeBaze extends Migration{
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->nullable();
         });
+        Schema::create('templejt',function(Blueprint $table){
+            $table->bigIncrements('id');
+            $table->string('naziv', 45);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->nullable();
+        });
+        Schema::create('vrsta_objekta',function(Blueprint $table){
+            $table->bigIncrements('id');
+            $table->string('naziv', 45);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->nullable();
+        });
+        Schema::create('grad',function(Blueprint $table){
+            $table->bigIncrements('id');
+            $table->string('naziv', 45);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->nullable();
+        });
+
+        Schema::create('objekat',function(Blueprint $table){
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('korisnik_id');
+            $table->foreign('korisnik_id')->references('id')->on('korisnik');
+            $table->unsignedBigInteger('templejt_id');
+            $table->foreign('templejt_id')->references('id')->on('templejt');
+            $table->unsignedBigInteger('grad_id');
+            $table->foreign('grad_id')->references('id')->on('grad');
+            $table->unsignedBigInteger('vrsta_objekta_id');
+            $table->foreign('vrsta_objekta_id')->references('id')->on('vrsta_objekta');
+            $table->string('naziv', 45);
+            $table->string('slug', 128);
+            $table->string('opis', 256)->nullable();
+            $table->string('adresa', 128)->nullable();
+            $table->string('galerija',250)->nullable();
+            $table->string('telefon',45)->nullable();
+            $table->string('email', 80)->nullable();
+            $table->string('x',45)->nullable();
+            $table->string('y',45)->nullable();
+            $table->string('z',45)->nullable();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->nullable();
+        });
+        Schema::create('vrsta_smestaja',function(Blueprint $table){
+            $table->bigIncrements('id');
+            $table->string('naziv', 45);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->nullable();
+        });
+        Schema::create('vrsta_kapaciteta',function(Blueprint $table){
+            $table->bigIncrements('id');
+            $table->string('naziv', 45);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->nullable();
+        });
+        Schema::create('dodaci',function(Blueprint $table){
+            $table->bigIncrements('id');
+            $table->string('naziv', 45);
+            $table->string('tag', 250)->nullable();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->nullable();
+        });
+        Schema::create('smestaj',function(Blueprint $table){
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('objekat_id');
+            $table->foreign('objekat_id')->references('id')->on('objekat');
+            $table->unsignedBigInteger('vrsta_smestaja_id');
+            $table->foreign('vrsta_smestaja_id')->references('id')->on('vrsta_smestaja');
+            $table->unsignedBigInteger('vrsta_kapaciteta_id');
+            $table->foreign('vrsta_kapaciteta_id')->references('id')->on('vrsta_kapaciteta');
+            $table->string('dodaci', 250)->nullable();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->nullable();
+        });
+        Schema::create('rezervacija',function(Blueprint $table){
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('korisnik_id');
+            $table->foreign('korisnik_id')->references('id')->on('korisnik');
+            $table->unsignedBigInteger('smestaj_id');
+            $table->foreign('smestaj_id')->references('id')->on('smestaj');
+            $table->date('datum_prijave')->nullable();
+            $table->date('datum_odjave')->nullable();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->nullable();
+        });
+
     }
 
 
     public function down(){
-        Schema::drop('prava_pristupa');
+        Schema::drop('rezervacija');
+        Schema::drop('smestaj');
+        Schema::drop('dodaci');
+        Schema::drop('vrsta_kapaciteta');
+        Schema::drop('vrsta_smestaja');
+        Schema::drop('objekat');
+        Schema::drop('grad');
+        Schema::drop('vrsta_objekta');
+        Schema::drop('templejt');
         Schema::drop('korisnik');
+        Schema::drop('prava_pristupa');
     }
 }
